7e781dc07ba20494faae455ad1b9b03ecbc2831c
liliguo <liliguo@mafengwo.com>
2020-05-13 13:14:20 +0800

    测试一下

---
+++ StringTest/src/com/liliguo/string/test/code/Main1.java
@@ -0,0 +1,14 @@
+package com.liliguo.string.test.code;
+
+/**
+ * @Author:liliguo
+ * @Description:
+ * @Date:Created in 1:10 下午 2020/5/13
+ * 测试一下
+ */
+public class Main1 {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
